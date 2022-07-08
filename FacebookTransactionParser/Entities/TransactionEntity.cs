9ccce100653ebb2ccb0097bd7482c8bb7766fd51
jrtrevino <j.trevino95@icloud.com>
2022-07-07 20:07:07 -0700

    Changed data types for raw transaction entity

---
+++ FacebookTransactionParser/Entities/TransactionEntity.cs
@@ -0,0 +1,52 @@
+﻿namespace FacebookTransactionParser.Entities
+{
+    using CsvHelper.Configuration.Attributes;
+
+    public class TransactionEntity
+    {
+        [Name("Order ID")]
+        public long OrderId { get; set; }
+
+        [Name("Date")]
+        public string? Date { get; set; }
+
+        [Name("Buyer")]
+        public string? Buyer { get; set; }
+
+        [Name("Price")]
+        public string? Price { get; set; }
+
+        [Name("Tax")]
+        public string? Tax { get; set; }
+
+        [Name("Shipping")]
+        public string? ShippingCost { get; set; }
+
+        [Name("Shipping Type")]
+        public string? ShippingType { get; set; }
+
+        [Name("Products")]
+        public string? Product { get; set; }
+
+        [Name("Quantity")]
+        public int Quantity { get; set; }
+
+        [Name("Street 1")]
+        public string? Street1 { get; set; }
+
+        [Name("Street 2")]
+        public string? Street2 { get; set; }
+
+        [Name("City")]
+        public string? City { get; set; }
+
+        [Name("State")]
+        public string? State { get; set; }
+
+        [Name("ZIP Code")]
+        public string? ZipCode { get; set; }
+
+        [Name("Order Status")]
+        public string? OrderStatus { get; set; }
+    }
+}
